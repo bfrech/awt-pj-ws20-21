89e947ba214f3d1c5177d5a799bf156c714a44a0
Anna Mockenhaupt <annamockenhaupt@Annas-MacBook-Pro.local>
2020-11-29 22:32:47 +0100

    set up and first component

---
+++ dash-if-reference-player/src/app/components/video-configuration/video-configuration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-configuration',
+  templateUrl: './video-configuration.component.html',
+  styleUrls: ['./video-configuration.component.css']
+})
+export class VideoConfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
