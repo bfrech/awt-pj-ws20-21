43fae81c6a5d1215ff6e1f63feb5246014e790a1
Berit Frech <berit.frech@campus.tu-berlin.de>
2021-03-03 09:57:14 +0100

    Change NONE to UNASSIGNED

--- dash-if-reference-player/src/app/components/video-configuration/video-configuration.component.ts
+++ dash-if-reference-player/src/app/components/video-configuration/video-configuration.component.ts
@@ -124,7 +124,7 @@ export class VideoConfigurationComponent implements OnInit {
     // Map to custom groups
     const withGroups: any[] = [];
     Object.entries(res).forEach(setting => {
-      if ( this.findGroup(setting[0]) === 'NONE') { return; }
+      if ( this.findGroup(setting[0]) === 'UNASSIGNED') { return; }
       withGroups.push([this.findGroup(setting[0]), setting[0], setting[1]]);
     });
 
